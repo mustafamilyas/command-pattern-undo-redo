7eea124ee4e1e6e5d1dbc6b9b22c738db313d337
Muhamad Ilyas Mustafa <emailpribadiilyas@gmail.com>
2023-06-17 16:03:13 +0700

    add Command utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,74 @@
-import "./App.css";
+import { CSSProperties, useState } from "react";
+import {
+  BoldCommand,
+  Command,
+  CommandUtils,
+  ItalicCommand,
+  UnderlineCommand,
+} from "./command";
+import styles from "./App.module.css";
 
-function App() {
+function useHistoryManager<T>() {
+  const [forwardHistory, setForwardHistory] = useState<Command<T>[]>([]);
+  const [backHistory, setBackHistory] = useState<Command<T>[]>([]);
+
+  const executeCommand = async (command: Command<T>) => {
+    // clear forward history
+    setForwardHistory([]);
+    await command.execute();
+    setBackHistory((prev) => [...prev, command]);
+  };
+  const redo = async () => {
+    if (!forwardHistory.length) return;
+    const topRedoCommand = forwardHistory[forwardHistory.length - 1];
+    await topRedoCommand.execute();
+    setForwardHistory((prev) => prev.slice(0, -1));
+    setBackHistory((prev) => [...prev, topRedoCommand]);
+  };
+  const undo = async () => {
+    if (!backHistory.length) return;
+    const topUndoCommand = backHistory[backHistory.length - 1];
+    await topUndoCommand.undo();
+    setBackHistory((prev) => prev.slice(0, -1));
+    setForwardHistory((prev) => [...prev, topUndoCommand]);
+  };
+  return {
+    forwardHistory,
+    backHistory,
+    executeCommand,
+    redo,
+    undo,
+  };
+}
+
+export default function App() {
+  const [styles, setStyles] = useState<CSSProperties>({});
+  const utils = { styles, setStyles };
+  const { executeCommand, redo, undo } = useHistoryManager<CommandUtils>();
+
+  const setTextToItalic = async () => {
+    const italicCommand = new ItalicCommand(utils);
+    await executeCommand(italicCommand);
+  };
+
+  const setTextToBold = async () => {
+    const boldCommand = new BoldCommand(utils);
+    await executeCommand(boldCommand);
+  };
+
+  const setTextToUnderline = async () => {
+    const underlineCommand = new UnderlineCommand(utils);
+    await executeCommand(underlineCommand);
+  };
+
   return (
     <div className="App">
-      <p>This is text</p>
+      <p style={styles}>Hello from react!</p>
+      <button onClick={setTextToItalic}>italic</button>
+      <button onClick={setTextToBold}>bold</button>
+      <button onClick={setTextToUnderline}>underline</button>
+      <button onClick={undo}>undo</button>
+      <button onClick={redo}>redo</button>
     </div>
   );
 }
-
-export default App;
---
+++ src/command.ts
@@ -0,0 +1,123 @@
+import { CSSProperties, Dispatch } from "react";
+
+export interface CommandUtils {
+  styles: CSSProperties;
+  setStyles: Dispatch<React.SetStateAction<CSSProperties>>;
+}
+
+export class Command<T> {
+  utils: T;
+  constructor(utils: T) {
+    this.utils = utils;
+  }
+  execute() {}
+  undo() {}
+  getInfo() {
+    return "This is basic command";
+  }
+}
+
+export class ItalicCommand<T extends CommandUtils> extends Command<T> {
+  prevFontStyle?: CSSProperties["fontStyle"];
+  constructor(utils: T) {
+    super(utils);
+    this.prevFontStyle = utils.styles.fontStyle;
+  }
+
+  getNextStyle() {
+    if (this.prevFontStyle === "italic") {
+      return "normal";
+    }
+    return "italic";
+  }
+
+  execute() {
+    const nextFontStyle = this.getNextStyle();
+    this.utils.setStyles((prevStyles) => ({
+      ...prevStyles,
+      fontStyle: nextFontStyle,
+    }));
+  }
+
+  undo() {
+    this.utils.setStyles((prevStyles) => ({
+      ...prevStyles,
+      fontStyle: this.prevFontStyle,
+    }));
+  }
+
+  getInfo() {
+    return "Italic command : Change into " + this.getNextStyle();
+  }
+}
+
+export class BoldCommand<T extends CommandUtils> extends Command<T> {
+  prevFontWeight?: CSSProperties["fontWeight"];
+  constructor(utils: T) {
+    super(utils);
+    this.prevFontWeight = utils.styles.fontWeight;
+  }
+
+  getNextStyle() {
+    if (
+      this.prevFontWeight === "bold" ||
+      (typeof this.prevFontWeight === "number" && this.prevFontWeight >= 700)
+    ) {
+      return "normal";
+    }
+    return "bold";
+  }
+
+  execute() {
+    const nextFontStyle = this.getNextStyle();
+    this.utils.setStyles((prevStyles) => ({
+      ...prevStyles,
+      fontWeight: nextFontStyle,
+    }));
+  }
+
+  undo() {
+    this.utils.setStyles((prevStyles) => ({
+      ...prevStyles,
+      fontWeight: this.prevFontWeight,
+    }));
+  }
+
+  getInfo() {
+    return "Bold Command : Change into " + this.getNextStyle();
+  }
+}
+
+export class UnderlineCommand<T extends CommandUtils> extends Command<T> {
+  prevTextDecoration?: CSSProperties["textDecoration"];
+  constructor(utils: T) {
+    super(utils);
+    this.prevTextDecoration = utils.styles.textDecoration;
+  }
+
+  getNextStyle() {
+    if (this.prevTextDecoration === "underline") {
+      return "unset";
+    }
+    return "underline";
+  }
+
+  execute() {
+    const nextFontStyle = this.getNextStyle();
+    this.utils.setStyles((prevStyles) => ({
+      ...prevStyles,
+      textDecoration: nextFontStyle,
+    }));
+  }
+
+  undo() {
+    this.utils.setStyles((prevStyles) => ({
+      ...prevStyles,
+      textDecoration: this.prevTextDecoration,
+    }));
+  }
+
+  getInfo() {
+    return "Underline command : Change into " + this.getNextStyle();
+  }
+}
